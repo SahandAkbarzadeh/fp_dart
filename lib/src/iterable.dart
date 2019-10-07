b0303f23eb9c9ce7e10904fe3483eca305479b05
Sahandevs <sahandevs@gmail.com>
2019-10-07 15:19:56 +0330

    implement allTrue, allFalse, Iterable<bool>.all

--- lib/src/iterable.dart
+++ lib/src/iterable.dart
@@ -8,3 +8,21 @@ extension FunctionalList<T> on Iterable<T> {
     return true;
   }
 }
+
+extension FunctionalListBool on Iterable<bool> {
+  bool all() {
+    for (var element in this) {
+      if (!element) return false;
+    }
+    return true;
+  }
+
+  bool allTrue() => this.all();
+
+  bool allFalse() {
+    for (var element in this) {
+      if (element) return false;
+    }
+    return true;
+  }
+}
--- test/iterable_test.dart
+++ test/iterable_test.dart
@@ -1,16 +1,34 @@
 import 'package:test/test.dart';
 import 'package:fp_dart/fp_dart.dart';
-
+const orderedNumber1to5 = [1, 2, 3, 4, 5];
+const allTrue = [true, true, true, true, true];
+const someTrue = [true, true, false, true, true];
+const someTrue2 = [false, true, false, true, true];
+const allFalse = [false, false, false];
 main() {
+  test('test iterable<T>.all', () {
 
-  test('test iterable.all', () {
-    var a = [1,2,3,4,5];
-    expect(a.all((element) => element > 0), true);
-    expect(a.all((element) => element < 6), true);
-    expect(a.all((element) => element <= 6), true);
+    expect(orderedNumber1to5.all((element) => element > 0), true);
+    expect(orderedNumber1to5.all((element) => element < 6), true);
+    expect(orderedNumber1to5.all((element) => element <= 6), true);
 
-    expect(a.all((element) => element == 6), false);
-    expect(a.all((element) => element == 1), false);
+    expect(orderedNumber1to5.all((element) => element == 6), false);
+    expect(orderedNumber1to5.all((element) => element == 1), false);
   });
 
+  test('test iterable<bool>.all', () {
+    expect(allTrue.all(), true);
+  });
+
+  test('test iterable<bool>.allTrue/allFalse', () {
+    expect(allTrue.allTrue(), true);
+    expect(allFalse.allTrue(), false);
+    expect(someTrue.allTrue(), false);
+    expect(someTrue2.allTrue(), false);
+
+    expect(allTrue.allFalse(), false);
+    expect(allFalse.allFalse(), true);
+    expect(someTrue.allFalse(), false);
+    expect(someTrue2.allFalse(), false);
+  });
 }
